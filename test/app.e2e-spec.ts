b7a415ea77b77b676d8bf0dd3721d112b2d54fa4
ajay <ajayzyan100@gmail.com>
2024-06-22 14:26:39 +0530

    refactor: unnecessary files removed

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,23 +1,18 @@
-import type { INestApplication } from '@nestjs/common';
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
-import * as request from 'supertest';
 
 import { AppModule } from '@/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('AppModule', () => {
+  let app: TestingModule;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    app = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+  it('should be defined', () => {
+    expect(app).toBeDefined();
   });
 });
