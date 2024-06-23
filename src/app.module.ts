e5b88906bd3f6d9c610c291804f8c3d8e814b7ac
ajay <ajayzyan100@gmail.com>
2024-06-23 11:35:31 +0530

    refactor: feature module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { LoggerModule } from '@/logger/logger.module';
-import { HealthModule } from '@/modules/health/health.module';
+import { FeatureModules } from '@/modules/feature.module';
 
 @Module({
-  imports: [LoggerModule, HealthModule],
+  imports: [LoggerModule, FeatureModules],
   controllers: [],
   providers: [],
 })
