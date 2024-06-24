d193f780fd41e1499622441baf8aa5af8bfc0798
ajay <ajayzyan100@gmail.com>
2024-06-24 09:46:31 +0530

    fix: database connection secured using relaxed ssl

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -6,6 +6,11 @@ import type { DatabaseOptions } from '@/database/utils';
 import { DatabaseService } from './database.service';
 import { ConfigurableDatabaseModule, CONNECTION_POOL, DATABASE_OPTIONS } from './utils';
 
+/**
+ * Global DatabaseModule for setting up the PostgreSQL connection pool.
+ * This module uses dependency injection to provide a configured connection pool
+ * and a DatabaseService to other modules in the application.
+ */
 @Global()
 @Module({
   exports: [DatabaseService],
@@ -15,17 +20,19 @@ import { ConfigurableDatabaseModule, CONNECTION_POOL, DATABASE_OPTIONS } from '.
       provide: CONNECTION_POOL,
       inject: [DATABASE_OPTIONS],
       useFactory: async (databaseOptions: DatabaseOptions) => {
-        const logger = new Logger('DatabaseModule');
+        const logger = new Logger();
 
+        // Create a new PostgreSQL connection pool with the given options
         const pool = new Pool({
           host: databaseOptions.host,
           port: databaseOptions.port,
           user: databaseOptions.user,
           password: databaseOptions.password,
           database: databaseOptions.database,
+          ssl: { rejectUnauthorized: false }, // Use SSL with relaxed security (not verifying server certificate)
         });
 
-        // Test the connection
+        // Test the database connection by running a simple query
         await pool.query('SELECT 1');
 
         logger.log('🚀 Database connection established successfully.');
